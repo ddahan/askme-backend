8dc9f8b080af73e17676d0fe03faa0008e6fcf55
David Dahan <beaucroco@gmail.com>
2016-05-29 21:43:53 +0000

    Modif modelisation

--- multiproject/freelanzer/models.py
+++ multiproject/freelanzer/models.py
@@ -11,6 +11,9 @@ class BankAccount(models.Model):
     iban = models.CharField(_("IBAN"), max_length=34)
     bic = models.CharField(_("BIC"), max_length=12)
 
+    def __str__(self):
+        return self.iban
+
 
 class Company(models.Model):
     """
@@ -40,7 +43,12 @@ class Company(models.Model):
     type = models.CharField(_("Type"), choices=COMPANY_TYPE_CHOICES)
     logo = models.CharField(_("URL du logo"))
     address = models.CharField(_("Adresse"), max_length=1024)
-    siret = models.CharField(_("Début"), max_length=256)
+    siret = models.CharField(_("SIRET"), max_length=256)
+    siren = models.CharField(_("SIREN"), max_length=256)
+    bank_account = models.ForeignKey(BankAccount)
+
+    def __str__(self):
+        return self.name
 
 
 class Freelance(models.Model):
@@ -51,6 +59,9 @@ class Freelance(models.Model):
     user = models.ForeignKey(User)
     company = models.ForeignKey(Company, null=True, blank=True)
 
+    def __str__(self):
+        return self.user
+
 
 class Client(models.Model):
     """
@@ -59,6 +70,9 @@ class Client(models.Model):
 
     user = models.ForeignKey(User)
     company = models.ForeignKey(Company)
+
+    def __str__(self):
+        return self.user
 
 
 class Mission(models.Model):
@@ -69,18 +83,21 @@ class Mission(models.Model):
     FIXED_PRICE = 'FIXED_PRICE'  # Forfait
     TIMED_PRICE = 'TIMED_PRICE'  # Régie
 
-    MISSION_TYPE_CHOICES = (
+    MISSION_MODE_CHOICES = (
         (FIXED_PRICE, 'Forfait'),
         (TIMED_PRICE, 'Régie'),
     )
 
     start = models.DateField(_("Début"))
     end = models.DateField(_("Fin"), blank=True, null=True)
-    type = models.CharField(_("Type"), choices=MISSION_TYPE_CHOICES, max_length=256)
+    mode = models.CharField(_("Type"), choices=MISSION_MODE_CHOICES, max_length=256)
     performer = models.ForeignKey(Freelance)
     client = models.ForeignKey(Client)
     short_description = models.CharField(max_length=1024, blank=True)
     long_description = models.TextField()
+
+    def __str__(self):
+        return "{} - Du {} au {}".format(self.client.company, self.start, self.end)
 
 
 class WorkUnit(models.Model):
@@ -89,23 +106,34 @@ class WorkUnit(models.Model):
     # TODO: définir comment modéliser cette partie
     """
 
+    def __str__(self):
+        pass  # TODO
+
 
 class Clause(models.Model):
     """
-    Clause contractuelle
+    Clause contractuelle (peut être utilisée pour décrire les clauses existantes)
     """
+    # TODO: ajouter les category choices
 
     description = models.TextField(_("Description"))
+    category = models.CharField(_("Catégorie"), max_length=256)
 
 
 class Contract(models.Model):
     """
     Contrat de travail
+    # Idée : pouvoir rendre public pour aspect communautaire
     """
 
-    mission = models.ForeignKey(Mission)
+    created = models.DateField(_("Date d'édition"))
+    mission = models.ForeignKey(Mission, blank=True, null=True)
     clauses = models.ManyToManyField(Clause, through="ClauseInContract",
                                      through_fields=('contract', 'clause'))
+    signed = models.BooleanField(_("Signé ?"))
+
+    def __str__(self):
+        return "{} - {}".format(self.mission, self.created)
 
 
 class ClauseInContract(models.Model):
@@ -135,7 +163,7 @@ class Invoice(models.Model):
 
     CHECK = 'CHECK'
     TRANSFER = 'TRANSFER'
-    ONLINE_TIER = 'ONLINE_TIER' # Ex: Paypal
+    ONLINE_TIER = 'ONLINE_TIER'  # Ex: Paypal
     OTHER = 'OTHER'
 
     PAYMENT_MODE_CHOICES = (
@@ -156,6 +184,9 @@ class Invoice(models.Model):
     canceled = models.BooleanField(default=False)
     additional_clauses = models.ManyToManyField(Clause, through="ClauseInInvoice",
                                                 through_fields=('invoice', 'clause'))
+
+    def __str__(self):
+        return "{} - {} ({})".format(self.identifier, self.mission, self.issue_date)
 
 
 class Incident(models.Model):
